8f40765b6fe74bed693bb86b242df09353addcbc
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-07 15:55:54 +0100

    finished

--- index.js
+++ index.js
@@ -30,13 +30,74 @@ for (let v = 10 ; v >= -10; v--){
 
 
 
-//тема: МАСИВЫ
+//тема: МАССИВЫ
 //(переменная которая хранит несколько значений (список))
 
 let example1 = [];//нечего не содержит
 let example2 = [3, 4 , 1 , 54 , 33]; //состоит из 5 эл и эти эл явл числами
 
+let example3 = ["Hello" , 55 , true, null]; //может содержать любые типы данных
 
+let example4 = ["Ajara" , " elya" , "Nestana"];
+let example5 = ["Chui","Osh","Issyk-Kul","Talas","Batken","Jalal-Abad", ]
+
+console.log (example4);//что бы вывести массив
+console.log(example4[0]);//что бы вывести определенный эл (отчет идет от нуля) 
+console.log (example5[2]);
+
+example4.push("Zina");//что бы добавить новый элемент в массив
+console.log(example4);
+
+example4.push("Lola");
+example4.unshift("maiia");//добавляет с начала 
+console.log(example4);
+
+
+//вырезаем из массива
+let name1 = example4.pop();;//вырез с конца
+let name2 = example4.shift();// вырезать с начала
+console.log(example4);
+
+example4[0] = "Aika";//изменяем первый элемент 
+console.log(example4);
+
+//сколько эл находится в массиве
+console.log(example4.length);
+
+
+//как работать с массивами используя циклы
+
+console.log("Hello , my name is " + example4[0]);
+console.log("Hello, my name is  " + example4[1]);
+console.log("Hello , my name is " + example4[2]);
+
+for (let i = 0; i < example4.length ; i++){ //можем вывести любой массив
+    console.log("Hello, my name is " + example4[i] ); 
+}
+
+for (let i = 0 ; i < example5.length ; i++){
+    console.log ("My sity is " + example5[i]  + "i live here");
+}
+
+
+//виды циклов:
+for (let name of example4){
+    console.log ("Hello, i am " + name); //чаще используется
+}
+
+for(let i in example4){
+    let name = example4[i];
+    console.log("Hello , my name is " + name);
+}
+
+example4.forEach(function (name, i){//для каждого эл массива выполняет функцию 
+    console.log("Hello..." + name);
+}) ;
+
+//деструктивный метод цикла
+while(example4.length > 0){
+    console.log (example4.pop());//меняет массив
+}
 
 
 

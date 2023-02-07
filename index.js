1ad0640ca7832c3896edf657d1a95076b040c76d
Koshoeva Ajar <ajarka1334@gmail.com>
2023-02-07 14:20:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function  sum(a,b ){
+ let result = a + b;
+ console.log(result)   
+}
+let m = sum(5,5);
+  
+
+//другая форма
+function sum(a,b){
+    console.log (a + b);
+}
+
+let n = sum (6,6)
+
+
